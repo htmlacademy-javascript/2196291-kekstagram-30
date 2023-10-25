16dbdfe00b1995d57c1e47cbe7fb278f1bf98004
ESemenoB <semen_es@mail.ru>
2023-10-25 22:28:43 +0600

    Отрисуй меня полностью

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 import { getSimilarPicture } from './data.js';
 
+
+// import { similarPicture } from './data.js';
+import { addThumbnailsToContainer } from './rendering.js';
+
+const pictures = getSimilarPicture();
+const pictureContainer = document.querySelector('.pictures');
+
+addThumbnailsToContainer(pictures, pictureContainer);
+
+
 console.log (getSimilarPicture());
