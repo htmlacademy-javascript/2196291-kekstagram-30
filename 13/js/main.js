f6cb62d41e6ffac94a235841d6cd7930635523a3
Keks <keks@htmlacademy.ru>
2023-11-15 17:01:31 +0300

    :heavy_check_mark: Сборка #13

---
+++ 13/js/main.js
@@ -0,0 +1,25 @@
+// import { getSimilarPictures } from './data.js';
+import { addThumbnailsToContainer } from './rendering.js';
+import './form-opening.js';
+import './form-validation.js';
+export { renderBigPicture } from './big-picture.js';
+import { loadPictures } from './api.js';
+import { showErrorMessage} from './data.js';
+
+
+// const pictures = getSimilarPictures();
+
+
+const pictureContainer = document.querySelector('.pictures');
+
+
+async function bootstrap() {
+  try {
+    const pictures = await loadPictures();
+    addThumbnailsToContainer(pictures, pictureContainer);
+  } catch(error) {
+    showErrorMessage();
+  }
+
+}
+bootstrap();
---
+++ 13/js/rendering.js
@@ -0,0 +1,24 @@
+import { renderBigPicture } from './big-picture.js';
+const getTemplatePicture = () => document.querySelector('#picture').content.querySelector('.picture');
+
+
+const createUserPicture = ({ url, like, comments, description}) => {
+  const userPicture = getTemplatePicture().cloneNode(true);
+  userPicture.querySelector('.picture__img').src = url;
+  userPicture.querySelector('.picture__likes').textContent = like;
+  userPicture.querySelector('.picture__comments').textContent = comments.length;
+  // userPicture.dataset.userPictureId = id;
+  // добавляем обработчик события при колике на минеатюре отрисовывается большое изображение
+  userPicture.addEventListener('click', () => {
+    renderBigPicture({ url, description, like, comments });
+
+  });
+  return userPicture;
+};
+
+const addThumbnailsToContainer = (pictures, container) => {
+  const userPictures = pictures.map(createUserPicture);
+  container.append(...userPictures);
+};
+
+export { addThumbnailsToContainer };
